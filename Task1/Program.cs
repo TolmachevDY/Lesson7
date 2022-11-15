9fa2fae427b826c0d7435408b0d96c6a4cfdc527
Tolmachev DY <Tolmachev DY@DESKTOP-5RJHKAS>
2022-11-15 12:27:09 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static double TriangleS(int a, int b, int c)
+        {
+            //Console.WriteLine("Пришли {0} {1} {2}",a,b,c);
+            double p = (a + b + c) / 2.0;            
+            if (p * (p - a) * (p - b) * (p - c) <= 0) Console.WriteLine("Ошибка в вводе данных");
+            return Math.Pow(p * (p - a) * (p - b) * (p - c), 0.5);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите три стороны первого треугольника:");
+            int[] t1 = new int[3];
+            t1[0] = Convert.ToInt32(Console.ReadLine());
+            t1[1] = Convert.ToInt32(Console.ReadLine());
+            t1[2] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Площадь первого треугольника: {0:0.00}", TriangleS(t1[0], t1[1], t1[2]));
+
+            Console.WriteLine("\nВведите три стороны второго треугольника:");
+            int[] t2 = new int[3];
+            t2[0] = Convert.ToInt32(Console.ReadLine());
+            t2[1] = Convert.ToInt32(Console.ReadLine());
+            t2[2] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Площадь второго треугольника: {0:0.00}", TriangleS(t2[0], t2[1], t2[2]));
+
+            double st1, st2;
+            st1 = TriangleS(t1[0], t1[1], t1[2]);
+            st2 = TriangleS(t2[0], t2[1], t2[2]);
+            if (st1 > st2) Console.WriteLine("\nПлощадь первого треуголька больше");
+            else if (st1 < st2) Console.WriteLine("\nПлощадь второго треуголька больше");
+            else Console.WriteLine("Площади треугольников равны");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void GetParamCube(double a, out double v, out double s)
+        {
+            v = a * a * a;
+            s = a * a * 6;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сторону куба a");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double v, s;
+            v = s = 0;
+            GetParamCube(a, out v, out s);
+            Console.WriteLine("\nОбъём куба:{0:f2}\n" +
+                                "Площадь куба:{1:f2}", v, s);
+            Console.ReadKey();
+        }
+    }
+}
